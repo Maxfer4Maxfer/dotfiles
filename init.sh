a396be410f83d8fc21314c7d210a24a902571bc1
ferapontov.m <ferapontov.m@wildberries.ru>
2020-02-10 10:10:28 +0300

    fix mispelling

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -x
 
 DOTFILES_DIR="$HOME/.dotfiles"
 
-submodule update --init --recursive
+git submodule update --init --recursive
 
 #TODO
 #Preinstall packages:
@@ -10,5 +11,12 @@ submodule update --init --recursive
 #       https://github.com/ierton/xkb-switch
 
 # Vim
+if [ -f ~/.vimrc ]; then
+    rm ~/.vimrc
+fi
 ln -s $DOTFILES_DIR/.vimrc ~/.vimrc
+
+if [ -d ~/.vim/pack/plugins ]; then
+    rm ~/.vim/pack/plugins
+fi
 ln -s $DOTFILES_DIR/vim/pack/plugins ~/.vim/pack/plugins
