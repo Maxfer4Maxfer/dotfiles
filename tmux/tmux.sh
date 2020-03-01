cc7f75f4398f7f11b30229c788e41f82557da71d
Maxfer4Maxfer <maxfer4maxfer@gmail.com>
2020-03-01 13:27:04 +0300

    01.03.2020

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -3,25 +3,29 @@ set -x
 
 DOTFILES_DIR=$1
 
-######## tmux ########
-if [ -f ~/.tmux.conf ]; then
-    rm ~/.tmux.conf
+if [[ $DOTFILES_DIR == "" ]]; then
+    DOTFILES_DIR="$HOME/.dotfiles"
 fi
+
+file(){
+    if [ -e $1 ] || [ -L $1 ]; then
+        rm $1
+    fi
+}
+
+######## tmux ########
+file ~/.tmux.conf
 ln -s $DOTFILES_DIR/tmux/tmux.conf ~/.tmux.conf
 
 if [ ! -d ~/.tmux/plugins ]; then
     mkdir -p ~/.tmux/plugins
 fi
 
-if [ -f ~/.tmux/plugins/tpm ]; then
-    rm -rf ~/.tmux/plugins/tpm
-fi
+file ~/.tmux/plugins/tpm
 ln -s $DOTFILES_DIR/tmux/tpm ~/.tmux/plugins/tpm
 
 ~/.tmux/plugins/tpm/bin/install_plugins
 ~/.tmux/plugins/tpm/bin/update_plugins all
 
-if [ -f ~/.tmux/plugins/tmux-themepack/my.tmuxtheme ]; then
-    rm ~/.tmux/plugins/tmux-themepack/my.tmuxtheme
-fi
+file ~/.tmux/plugins/tmux-themepack/my.tmuxtheme
 ln -s $DOTFILES_DIR/tmux/my.tmuxtheme ~/.tmux/plugins/tmux-themepack/my.tmuxtheme
